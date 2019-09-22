298b758b9e641fb22adc9bbe86622132e4303b59
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-23 03:49:41 +0530

    Added item through input field to the database

--- src/MyApp/models.py
+++ src/MyApp/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class ToDoList(models.Model):
     name = models.CharField(max_length = 200)
+    added_date = models.DateTimeField(auto_now=True)
--- src/MyApp/views.py
+++ src/MyApp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
+from MyApp.models import ToDoList
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 
 @csrf_exempt
 def add_to_do(request):
-    print("todo")
+    #content = request.POST.get('search')
+    #Another method to do the same thing
+    content = request.POST['search']
+    ToDoList.objects.create(name=content)
     return HttpResponseRedirect('/')
