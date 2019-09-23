c92d7a0a72f8592f88b207b7ac8186805ce0a16d
Niketan E. Moon <niketanmoon@gmail.com>
2019-09-24 01:46:48 +0530

    Added GeoIP2 for learning and getting ip fuction

--- src/MyApp/models.py
+++ src/MyApp/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class ToDoList(models.Model):
     name = models.CharField(max_length = 200)
     added_date = models.DateTimeField(auto_now=True)
+    ip_address = models.CharField(max_length = 30, blank=True,null=True)
--- src/MyApp/views.py
+++ src/MyApp/views.py
@@ -3,7 +3,19 @@ from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from MyApp.models import ToDoList
 # Create your views here.
+
+def get_client_ip(request):
+    try:
+        x_forward = request.META.get("HTTP_X_FORWARDED_FOR")
+        if x_forward:
+            ip = x_forward.split(",")[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+    except:
+        ip=""
+    return ip
 def home(request):
+    print(request.META.get('REMOTE_ADDR'))
     items = ToDoList.objects.all().order_by('-added_date')
     context = {
         'items':items,
@@ -15,7 +27,7 @@ def add_to_do(request):
     #content = request.POST.get('search')
     #Another method to do the same thing
     content = request.POST['search']
-    ToDoList.objects.create(name=content)
+    ToDoList.objects.create(name=content,ip_address=get_client_ip(request))
     return HttpResponseRedirect('/')
 
 @csrf_exempt
